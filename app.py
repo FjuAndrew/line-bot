eb488a562a72b420ebf76d6f30d0aab6a4d7e9de
andrew.yen <zuzu8989@gmail.com>
2024-08-09 19:09:55 +0800

    更改linebot套件

--- app.py
+++ app.py
@@ -1,34 +1,39 @@
 from flask import Flask, request, abort
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import *
-import os
+from linebot.v3 import WebhookHandler
+from linebot.v3.exceptions import InvalidSignatureError
+from linebot.v3.messaging import Configuration, ApiClient, MessagingApi, ReplyMessageRequest, TextMessage
+from linebot.v3.webhooks import MessageEvent, TextMessageContent
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(access_token='UZ83W/SnkQoLRE3cyBmA4lmjqvr1sWUMr480jsOSwcUvkgYaBS6V7Tr8IrwZEj4iTZyqgx9X/pTUYg9U4Ayai4GdL0cS3umfQprdmWH+kCs4zhzfGItLdYwSHdXGoANpwR8L1V5QKg0nyv2fmjczawdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('f4ff46da7257b20d8b6a663ff2557927')
-
-
+configuration = Configuration(access_token='f4ff46da7257b20d8b6a663ff2557927')
+handler = WebhookHandler('UZ83W/SnkQoLRE3cyBmA4lmjqvr1sWUMr480jsOSwcUvkgYaBS6V7Tr8IrwZEj4iTZyqgx9X/pTUYg9U4Ayai4GdL0cS3umfQprdmWH+kCs4zhzfGItLdYwSHdXGoANpwR8L1V5QKg0nyv2fmjczawdB04t89/1O/w1cDnyilFU=')
 
 
 @app.route("/callback", methods=['POST'])
 def callback():
+    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
+
+    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
+
+    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
+        app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
+
     return 'OK'
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
 
-import os
+@handler.add(MessageEvent, message=TextMessageContent)
+def handle_message(event):
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+        line_bot_api.reply_message_with_http_info( ReplyMessageRequest( reply_token=event.reply_token, messages=[TextMessage(text=event.message.text)]))
+
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()

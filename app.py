4d546ca3f8342ea2f58d1a4262574d0ce89ae5b3
andrew.yen <zuzu8989@gmail.com>
2024-08-09 19:02:05 +0800

    更改line token

--- app.py
+++ app.py
@@ -6,8 +6,10 @@ import os
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(os.environ['UZ83W/SnkQoLRE3cyBmA4lmjqvr1sWUMr480jsOSwcUvkgYaBS6V7Tr8IrwZEj4iTZyqgx9X/pTUYg9U4Ayai4GdL0cS3umfQprdmWH+kCs4zhzfGItLdYwSHdXGoANpwR8L1V5QKg0nyv2fmjczawdB04t89/1O/w1cDnyilFU='])
-handler = WebhookHandler(os.environ['f4ff46da7257b20d8b6a663ff2557927'])
+line_bot_api = LineBotApi(access_token='UZ83W/SnkQoLRE3cyBmA4lmjqvr1sWUMr480jsOSwcUvkgYaBS6V7Tr8IrwZEj4iTZyqgx9X/pTUYg9U4Ayai4GdL0cS3umfQprdmWH+kCs4zhzfGItLdYwSHdXGoANpwR8L1V5QKg0nyv2fmjczawdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('f4ff46da7257b20d8b6a663ff2557927')
+
+
 
 
 @app.route("/callback", methods=['POST'])

4a52016059d2fa1806b8a8011372d4bbbc143d62
FjuAndrew <104815551+FjuAndrew@users.noreply.github.com>
2024-08-10 22:23:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,45 +10,31 @@ from linebot.models import *
 app = Flask(__name__)
 channel_secret = os.getenv('LINE_CHANNEL_SECRET')
 channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN')
-configuration = Configuration(access_token=channel_access_token)
+line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
 
 
 @app.route("/", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-
-    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
-    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
-        app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
-
     return 'OK'
 
-
-@handler.add(MessageEvent, message=TextMessageContent)
+@handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    with ApiClient(configuration) as api_client:
-        line_bot_api = MessagingApi(api_client)
-        line_bot_api.reply_message_with_http_info( ReplyMessageRequest( reply_token=event.reply_token, messages=[TextMessage(text=event.message.text)]))
+    message = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token, message)
 
-
-@ handler.add(MessageEvent, message=TextMessageContent)
-def choose_food(event):
+@ handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
     if "吃什麼" in event.message.text:
-        eat = random.choice(['水餃', '小7', '火鍋', '炒飯', '拉麵', '陽春麵'])
-        reply_message_request = ReplyMessageRequest(
-            reply_token=event.reply_token,
-            messages=[TextSendMessage(text=eat)]
-        )
-        line_bot_api.reply_message_with_http_info(reply_message_request)
+        eat = random.choice(['水餃', '小7', '火鍋', '炒飯','拉麵','陽春麵'])
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=eat))
 
 
 

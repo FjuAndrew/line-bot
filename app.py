f375e37b5e80ca37b833193ab0bc7a0e2bba8b60
FjuAndrew <104815551+FjuAndrew@users.noreply.github.com>
2024-08-15 17:46:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, request, abort
 from linebot.v3 import WebhookHandler
-from linebot.v3.exceptions import InvalidSignatureError,LineBotApiError
+from linebot.v3.exceptions import InvalidSignatureError
 from linebot.v3.messaging import Configuration, ApiClient, MessagingApi, ReplyMessageRequest, TextMessage
 from linebot.v3.webhooks import MessageEvent, TextMessageContent
+from linebot.exceptions import LineBotApiError
 import os 
 import gunicorn
 import random

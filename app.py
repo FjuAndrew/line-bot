b74cdb04a4b68518f60cc33c7f87564b8efd8f53
FjuAndrew <104815551+FjuAndrew@users.noreply.github.com>
2024-08-16 11:27:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 from flask import Flask, request, abort
 from linebot.v3 import WebhookHandler
+# from linebot import LineBotApi, WebhookHandler
 from linebot.v3.exceptions import InvalidSignatureError
 from linebot.v3.messaging import Configuration, ApiClient, MessagingApi, ReplyMessageRequest, TextMessage
 from linebot.v3.webhooks import MessageEvent, TextMessageContent
+from linebot.v3.messaging.models import TemplateMessage, ButtonsTemplate, MessageAction
 from linebot.exceptions import LineBotApiError
 import os 
 import gunicorn
 import random
-from linebot.models import PostbackAction,URIAction, MessageAction, TemplateSendMessage, ButtonsTemplate
+# from linebot.models import PostbackAction,URIAction, MessageAction, TemplateSendMessage, ButtonsTemplate
 app = Flask(__name__)
 
 
 channel_secret = os.getenv('LINE_CHANNEL_SECRET')
 channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN')
 configuration = Configuration(access_token=channel_access_token)
+line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
 
 
@@ -39,23 +42,23 @@ def callback():
 @handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
     with ApiClient(configuration) as api_client:
-        line_bot_api = MessagingApi(api_client)
+        line_bot_apiv3 = MessagingApi(api_client)
         if "吃什麼" in event.message.text:
             choose_food(event)
         elif event.message.text == '!按鈕樣板':
             button_template(event) 
         else:
-            line_bot_api.reply_message_with_http_info( ReplyMessageRequest( reply_token=event.reply_token, messages=[TextMessage(text=event.message.text)]))
+            line_bot_apiv3.reply_message_with_http_info( ReplyMessageRequest( reply_token=event.reply_token, messages=[TextMessage(text=event.message.text)]))
 
 def choose_food(event):
         with ApiClient(configuration) as api_client:
-            line_bot_api = MessagingApi(api_client)
+            line_bot_apiv3 = MessagingApi(api_client)
             eat = random.choice(['八方', '7-11', '滷肉飯', '涼麵','燒臘','賣噹噹'])
-            line_bot_api.reply_message_with_http_info( ReplyMessageRequest( reply_token=event.reply_token, messages=[TextMessage(text=eat)]))
+            line_bot_apiv3.reply_message_with_http_info( ReplyMessageRequest( reply_token=event.reply_token, messages=[TextMessage(text=eat)]))
 
 def button_template(event):
     with ApiClient(configuration) as api_client:
-        line_bot_api = MessagingApi(api_client)
+        line_bot_apiv3 = MessagingApi(api_client)
         buttons_template = ButtonsTemplate(
                 title='按鈕樣板',
                 thumbnail_image_url='https://imgur.com/a/eKV35K4',
@@ -72,7 +75,8 @@ def button_template(event):
             template=buttons_template
         )
         try:
-            line_bot_api.reply_message_with_http_info(ReplyMessageRequest(
+            # line_bot_api.reply_message('<REPLY_TOKEN>', template_message)
+            line_bot_apiv3.reply_message_with_http_info(ReplyMessageRequest(
                 reply_token=event.reply_token,
                 messages=[template_message] # 加上關鍵字參數
             ))

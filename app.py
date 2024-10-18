713dc40290b01f2bfb047ce513868b1610e260c4
FjuAndrew <104815551+FjuAndrew@users.noreply.github.com>
2024-10-18 15:31:08 +0800

    Update app.py
    修改錯誤

--- app.py
+++ app.py
@@ -141,13 +141,12 @@ def button_template(event,user_input_for_search):
             print(f"Error: {e}")
 def send_line_message():
     try:
-        ga_data = get_ga_data()
         line_bot_apiv3.push_message(
             PushMessageRequest(
                 to=event.source.user_id,
                 messages=[TextMessage(text='Hello! Damn SoB')]
             ))
-            print("測試主動發訊息")
+        print("測試主動發訊息")
     except Exception as e:
         print(f'Error: {e}')
         

80d82d74102a0dae612c12226365d590510c2d71
FjuAndrew <104815551+FjuAndrew@users.noreply.github.com>
2024-10-24 17:48:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -69,9 +69,9 @@ handler = WebhookHandler(channel_secret)
 @app.route("/health", methods=['HEAD', 'GET'])
 def health_check():
     now = datetime.now()
-    target_hour = 17  # 指定小时
+    target_hour = 18  # 指定小时
 
-    if now.hour == target_hour and now.minute == target_minute:
+    if now.hour == target_hour:
         send_line_message()
         time.sleep(3600) 
     return 'OK', 200
